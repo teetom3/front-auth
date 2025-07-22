ebf3d7347f2877b7a6687ed4f2e7c01410b58cde
thomas <thomas.zeryouh@goons.fr>
2025-07-22 11:12:03 +0200

    first commit

---
+++ src/components/OfferList.jsx
@@ -0,0 +1,28 @@
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router";
+
+const OfferList = ({ offers }) => {
+  return (
+    <Container className="mt-4">
+      <h1 className="mb-4">Liste des Produits</h1>
+      <Row>
+        {offers.map((offer) => (
+          <Col md={4} sm={6} xs={12} key={offer.id} className="mb-4">
+            <Link to={`/offre/${offer.id}`} className="text-decoration-none">
+              <Card>
+                <Card.Body>
+                  <Card.Title>{offer.name}</Card.Title>
+                  <Card.Text>
+                    <strong>{offer.price}€</strong>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default OfferList;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import {
+  Form,
+  Button,
+  Container,
+  Card,
+  Row,
+  Col,
+  Alert,
+} from "react-bootstrap";
+import { useNavigate } from "react-router";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://offers-api.digistos.com/api/auth/register",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.message || "Erreur lors de l'inscription");
+      }
+      console.log("inscription réussi");
+      navigate("/connexion");
+    } catch (error) {
+      setError(
+        "Une erreur est survenue lors de l'inscription: " + error.message
+      );
+      console.error("Erreur d'inscription:", error.message);
+    } finally {
+      setIsLoading(false);
+    }
+    // Handle signup logic here
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center min-vh-100">
+      <Row className="w-100 justify-content-center">
+        <Col xs={12} sm={8} md={6} lg={4}>
+          <Card className="p-4 shadow-lg">
+            {error && (
+              <Alert variant="danger" className="mb-3">
+                {error}
+              </Alert>
+            )}
+            <h1 className="text-center mb-4">Créer un compte</h1>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Nom</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-4" controlId="formPassword">
+                <Form.Label>Mot de passe</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+
+              <Button
+                variant="primary"
+                type="submit"
+                className="w-100"
+                disabled={isLoading}
+              >
+                {isLoading ? "Inscription en Cours ..." : "S'inscrire"}
+              </Button>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Register;
