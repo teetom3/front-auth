eb97df3850aed59edde2763ef5286ded7c0bb571
thomas <thomas.zeryouh@goons.fr>
2025-07-23 10:22:22 +0200

    modif cerif token

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -5,30 +5,39 @@ const Logout = () => {
   const navigate = useNavigate();
   useEffect(() => {
     const handleLogout = async () => {
+      const auth = JSON.parse(localStorage.getItem("auth"));
+      const token = auth?.token;
       // (1) Appel API pour notifier la déconnexion
+
       try {
-        const response = await fetch(
-          "https://offers-api.digistos.com/api/auth/logout",
-          {
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              Authorization: `Bearer ${
-                JSON.parse(localStorage.getItem("auth"))?.token
-              }`,
-            },
+        if (token) {
+          const response = await fetch(
+            "https://offers-api.digistos.com/api/auth/logout",
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${
+                  JSON.parse(localStorage.getItem("auth"))?.token
+                }`,
+              },
+            }
+          );
+          const data = await response.json();
+          if (!response.ok) {
+            throw new Error(data.message || "Erreur lors de la déconnexion");
           }
-        );
-        const data = await response.json();
-        if (!response.ok) {
-          throw new Error(data.message || "Erreur lors de la déconnexion");
+        } else {
+          throw new Error("Missing token");
         }
+
         // (2) Suppression du token côté frontend
-        localStorage.removeItem("auth");
+
         // (3) Redirection vers la page de login
       } catch (error) {
         console.error(`${error.message}`);
       } finally {
+        localStorage.removeItem("auth");
         navigate("/connexion");
       }
     };
